64eb53994827cc199c51bab4698cbdfb8239ccf3
gqq <814359335@qq.com>
2018-09-17 14:46:38 +0800

    set nested circle list render

--- travel/src/pages/detail/Detail.vue
+++ travel/src/pages/detail/Detail.vue
@@ -2,18 +2,41 @@
   <div>
     <detail-header></detail-header>
     <detail-banner></detail-banner>
-    <div class="content"></div>
+    <!-- <detail-list :list="list"></detail-list> -->
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/DetailBanner'
 import DetailHeader from './components/DetailHeader'
+import DetailList from './components/DetailList'
 export default {
   name: 'Detail',
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
+  },
+  data () {
+    return {
+      list: [
+        {
+          title: '成人票',
+          children: [
+            {
+              title: '成人三日游',
+              children: [{
+                title: '某某旅游社提供'
+              }]
+            }
+          ]
+        }, {
+          title: '儿童票'
+        }, {
+          title: '团体票'
+        }
+      ]
+    }
   }
 }
 </script>
---
+++ travel/src/pages/detail/components/DetailList.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <div class="ticket-kind border-bottom"
+        v-for="(item, index) in list"
+        :key="index"
+    >
+      <span class="ticket-icon"></span>
+      {{item.title}}
+      <div class="children"
+        v-if="item.children"
+      >
+        <detail-list :list="item.children"></detail-list>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    list: Array
+  },
+  name: 'DetailList'
+}
+</script>
+
+<style lang="stylus" scoped>
+  .children
+    padding: 0 .2rem
+  .ticket-kind
+    width: 100%
+    line-height: .88rem
+    font-size: .32rem
+    align-items: center
+    padding: 0 .2rem
+    flex-flow: raw wrap
+    .ticket-icon
+      display: inline-block;
+      width: .36rem;
+      height: .36rem;
+      background: url(http://s.qunarzz.com/piao/image/touch/sight/detail.png) 0 -.45rem no-repeat;
+      margin-right: .05rem;
+      background-size: .4rem 3rem;
+</style>
